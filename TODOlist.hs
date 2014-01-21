b7599e0312267626de98360a1b9ef23e06325a9a
Paweł Szynkiewicz <pszynk@gmail.com>
2014-01-21 13:04:00 +0100

    przed StateT

--- TODOlist.hs
+++ TODOlist.hs
@@ -1,14 +1,41 @@
-import System.Console.Haskeline
+import Data.Time.Clock (getCurrentTime)
+import Control.Monad.Trans (liftIO)
+import System.Console.ANSI (Color(..)
+  , ColorIntensity(..)
+  , ConsoleLayer(..)
+  , SGR(..)
+  , Underlining(..)
+  , clearScreen
+  , setCursorPosition
+  , setSGRCode)
+
+import System.Console.Haskeline (InputT
+  , defaultSettings
+  , getInputLine
+  , runInputT
+  , setComplete
+  , outputStrLn)
+
+import Logic
+import Command
+
+__PROMPT :: String
+__PROMPT = "todo> "
 
 main :: IO ()
-main = runInputT defaultSettings loop
+main = do
+  time <- getCurrentTime
+  runInputT defaultSettings (interloop $ getEmptyStateData time)
+--main = runInputT defaultSettings (interloop $ getEmptyStateData time)
+  --where
+    --time = return getCurrentTime
+
+interloop :: TodoStateData -> InputT IO ()
+interloop sd = do
+  line <- getInputLine __PROMPT
+  case line of
+    Nothing -> return ()
+    Just "time" -> lift () >> interloop sd
+    Just cmdline -> liftIO (runCommand $ words cmdline) >> interloop sd
   where
-    loop :: InputT IO ()
-    loop = do
-      minput <- getInputLine "% "
-      case minput of
-        Nothing -> return ()
-        Just "quit" -> return ()
-        Just input -> do
-          outputStrLn $ "Input was: " ++ input
-          loop
+    now = getCurrentTime
